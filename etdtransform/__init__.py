83a786a53eda1ee889bbb1c57ef9c8a0e885d51c
Petra Izeboud <petra.izeboud@live.nl>
2025-03-04 14:34:51 +0100

    Add higher level imports in __init__

--- etdtransform/__init__.py
+++ etdtransform/__init__.py
@@ -1,3 +1,29 @@
 from etdtransform._config import options
 
-__all__ = ["options"]
+from . import (
+    _config,
+    aggregate,
+    calculated_columns,
+    impute,
+    knmi,
+    load_data,
+    vectorized_impute,
+)
+
+# Explicitly export modules and functions
+__all__ = [
+    # Modules
+    _config,
+    aggregate,
+    calculated_columns,
+    impute,
+    knmi,
+    load_data,
+    vectorized_impute,
+    # Specific imports from ettransform
+    "options",
+]
+
+from ._config import options
+
+
